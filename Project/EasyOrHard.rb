adff945571d0f4b263afeff13ed244f293848aa7
hidroo <hidr0.frbg@gmail.com>
2014-06-17 01:17:40 +0300

    Well I have some problems. One of the files returns only true or false, I do not know how to get the printed message between the two files.

---
+++ Project/EasyOrHard.rb
@@ -0,0 +1,27 @@
+hex = ["0x","0x"]
+
+def makingTheHard hex
+	Random.new.rand(4..8).times do
+		hex << Random.new.rand(1..14).to_s(16)
+	end
+end
+
+def makingTheEasy hex
+	ran = [Random.new.rand(0..15),0,0]
+	Random.new.rand(4..8).times do
+		hex << ran[Random.new.rand(0..2)].to_s(16)
+	end
+	
+end
+
+if ARGV[0].downcase == "easy"
+	2.times do |i|
+		makingTheEasy hex[i]
+	end
+elsif ARGV[0].downcase == "hard"
+	2.times do |i|
+		makingTheHard hex[i]
+	end
+end
+system("#{hex}")
+
--- Project/PrintingBasicCStuff.rb
+++ Project/PrintingBasicCStuff.rb
@@ -1,27 +1,31 @@
-params = [ARGV[0],ARGV[1],ARGV[2]]
-if(ARGV[2].to_s.downcase == "and")
-	Operator = "&"
-elsif ARGV[2].to_s.downcase == "or"
-	Operator = "|"
-elsif ARGV[2].to_s.downcase == "xor"
-	Operator = "^"
+level = ARGV[0]
+
+if(ARGV[1].to_s.downcase == "and")
+	operator = "&"
+elsif ARGV[1].to_s.downcase == "or"
+	operator = "|"
+elsif ARGV[1].to_s.downcase == "xor"
+	operator = "^"
 end
+ tempHex = system("ruby EasyOrHard.rb #{level}").to_s
+p "tempHex="
+p tempHex
 
 File.open("hello.c", "w") do |file|  
 	file <<
 	"
 	#include<stdio.h>
 	int main(){
-		int res = #{params[0]}#{Operator}#{params[1]};
+		int res = #{tempA}#{operator}#{tempB};
 		printf(\"0x\");
 		printf(\"%04x\",res);
-
 		return 0;
 	}
 	"
 end
 
-hello = system("gcc hello.c && ./a.out")
-print(hello.to_s.gsub("true",""))
-print("\n")
-system("rm hello.c a.out")
+# hello = system("gcc hello.c && ./a.out")
+# print(hello.to_s.gsub("true",""))
+# print("\n")
+# system("rm hello.c a.out")
+
